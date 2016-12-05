a2a2cee0ded7b2fdb0580a290904865301c9014c
Xavier Via <fernando.via@gmail.com>
2016-12-05 16:30:16 +0100

    Remove debugger, add Makefile 🛠

--- src/app.js
+++ src/app.js
@@ -15,7 +15,6 @@ export default class App extends Component {
 
   render () {
     const {dragging, sources} = this.state
-    debugger
 
     return <main
       className={dragging ? 'dragging' : ''}
